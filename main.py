60434d95bd01794fd7ef11329cf0c5c70455b699
Diego Villamil <dmvillamil@up.edu.ph>
2025-09-25 19:59:02 +0800

    Data from one page gets saved to dict

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+import random
 
 from asynciolimiter import Limiter
 from rnet import Client, Impersonate, Proxy
@@ -34,7 +35,9 @@ def parse_resp(response: str):
     return html
 
 
-async def fetch_data(client: Client, url: str, limiter: Limiter):
+async def fetch_data(client: Client, url: str, limiter: Limiter, proxies: list[Proxy]):
+    proxy_ind = random.randrange(0, len(proxies))
+    proxy = proxies[proxy_ind]
     task = limiter.wrap(client.get(url))
     return await task
 
@@ -45,10 +48,34 @@ async def main():
     client = create_client(proxy_list)
     url = "https://www.scrapethissite.com/pages/forms/?page_num=1"
 
-    response = await fetch_data(client, url, limiter)
+    response = await fetch_data(client, url, limiter, proxy_list)
     html = parse_resp(await response.text())
-    for node in html.body.css(".team"):
-        print(node.text())
+
+    data = {}
+    for node in html.body.css(".table .team"):
+        curr_row = node.css("td")
+        data[curr_row[0].text().strip()] = {
+            "year": curr_row[1].text().strip(),
+            "wins": curr_row[2].text().strip(),
+            "losses": curr_row[3].text().strip(),
+            "ot-losses": curr_row[4].text().strip(),
+            "win-pct": curr_row[5].text().strip(),
+            "goals-for": curr_row[6].text().strip(),
+            "goals-against": curr_row[7].text().strip(),
+            "goal-diff": curr_row[8].text().strip(),
+        }
+
+    for team, stats in data.items():
+        print(f"{team} Stats:")
+        print(f"Year: {stats['year']}")
+        print(f"Wins: {stats['wins']}")
+        print(f"Losses: {stats['losses']}")
+        print(f"Overtime Losses: {stats['ot-losses']}")
+        print(f"Win Percentage: {stats['win-pct']}")
+        print(f"Goals For: {stats['goals-for']}")
+        print(f"Goals Against: {stats['goals-against']}")
+        print(f"Goals difference: {stats['goal-diff']}")
+        print("-----------------------------------------------\n")
 
 
 if __name__ == "__main__":
